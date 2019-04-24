3a54cf0bb9ed6cd2d4bfc8f963cad491b32655f4
tomohalloran@outlook.com <tomohalloran@outlook.com>
2019-04-24 16:01:45 +0000

    Link articles to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_163904) do
+ActiveRecord::Schema.define(version: 2019_04_24_155850) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
